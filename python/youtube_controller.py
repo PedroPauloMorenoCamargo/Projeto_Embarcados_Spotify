f8d5d57be028b7b0f74dd8b4702a7a245cbc2656
PedroPauloMorenoCamargo <pedropmc1@al.insper.edu.br>
2023-03-16 09:56:55 -0300

    Testando pela Serial com Atmel

--- python/youtube_controller.py
+++ python/youtube_controller.py
@@ -14,7 +14,7 @@ class SerialControllerInterface:
     # byte 2 -> EOP - End of Packet -> valor reservado 'X'
 
     def __init__(self, port, baudrate):
-        self.ser = serial.Serial(port, baudrate=baudrate)
+        self.ser = serial.Serial('COM5', baudrate=baudrate)
         self.mapping = MyControllerMap()
         self.incoming = '0'
         pyautogui.PAUSE = 0  ## remove delay
@@ -31,13 +31,12 @@ class SerialControllerInterface:
         logging.debug("Received DATA: {}".format(data))
 
         if data == b'1':
-            print("datab1")
-            logging.info("KEYDOWN A")
-            pyautogui.keyDown(self.mapping.button['A'])
+            print("datab1 ")
+            pyautogui.keyDown(self.mapping.button['espaco'])
         elif data == b'0':
-            print("datab0")
-            logging.info("KEYUP A")
-            pyautogui.keyUp(self.mapping.button['A'])
+            print("datab0") 
+            logging.info("KEYUP ESPACO")
+            pyautogui.keyUp(self.mapping.button['espaco'])  
 
         self.incoming = self.ser.read()
 
@@ -56,20 +55,7 @@ class DummyControllerInterface:
 
 if __name__ == '__main__':
     interfaces = ['dummy', 'serial']
-    argparse = argparse.ArgumentParser()
-    argparse.add_argument('serial_port', type=str)
-    argparse.add_argument('-b', '--baudrate', type=int, default=115200)
-    argparse.add_argument('-c', '--controller_interface', type=str, default='serial', choices=interfaces)
-    argparse.add_argument('-d', '--debug', default=False, action='store_true')
-    args = argparse.parse_args()
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
-
-    print("Connection to {} using {} interface ({})".format(args.serial_port, args.controller_interface, args.baudrate))
-    if args.controller_interface == 'dummy':
-        controller = DummyControllerInterface()
-    else:
-        controller = SerialControllerInterface(port=args.serial_port, baudrate=args.baudrate)
+    controller = SerialControllerInterface(port="COM5", baudrate=115200)
 
     while True:
         controller.update()
