da074e808b59888580347aa893e35d386225ba74
PedroPauloMorenoCamargo <pedropmc1@al.insper.edu.br>
2023-04-05 03:53:07 -0300

    a

--- python/youtube_controller.py
+++ python/youtube_controller.py
@@ -3,10 +3,12 @@ import serial
 import argparse
 import time
 import logging
+import os
+import subprocess
 
 class MyControllerMap:
     def __init__(self):
-        self.button = {'espaco': 'space', 'control':'ctrl','direita':'right','esquerda':'left','shift':'shift','aumenta':'up','diminui': 'down'} # Fast forward (10 seg) pro Youtube # Fast forward (10 seg) pro Youtube
+        self.button = {'espaco': 'space', 'control':'ctrl','direita':'right','esquerda':'left','shift':'shift','aumenta':'up','diminui': 'down','Shuffle':'s','Refresh':'r'} # Fast forward (10 seg) pro Youtube # Fast forward (10 seg) pro Youtube
 
 class SerialControllerInterface:
     # Protocolo
@@ -21,7 +23,7 @@ class SerialControllerInterface:
     
     def update(self):
         ## Sync protocol
-        print("update")
+        print("update")   
         while self.incoming != b'X':
             self.incoming = self.ser.read()
             logging.debug("Received INCOMING: {}".format(self.incoming))
@@ -30,15 +32,26 @@ class SerialControllerInterface:
         data = self.ser.read()
         logging.debug("Received DATA: {}".format(data))
 
-        if data == b'1':
-            print("datab1 ")
+        if data == b'P':
+            print("Play")
             pyautogui.keyDown(self.mapping.button['espaco'])
-        elif data == b'0':
-            print("datab0") 
-            logging.info("KEYUP ESPACO")
-            pyautogui.keyUp(self.mapping.button['espaco'])  
-
+            pyautogui.keyUp(self.mapping.button['espaco'])
+        elif data == b'F':
+            print("Forward")
+            pyautogui.hotkey(self.mapping.button['control'],self.mapping.button['direita'])
+        elif data == b'B':
+            print("Backward")
+            pyautogui.hotkey(self.mapping.button['control'],self.mapping.button['esquerda'])
+        elif data == b'S':
+            print("Shuffle")
+            pyautogui.hotkey(self.mapping.button['control'],self.mapping.button['Shuffle'])
+        elif data == b'R':
+            print("Refresh")
+            pyautogui.hotkey(self.mapping.button['control'],self.mapping.button['Refresh'])
+    
         self.incoming = self.ser.read()
+
+    
 
 
 class DummyControllerInterface:
@@ -56,6 +69,9 @@ class DummyControllerInterface:
 if __name__ == '__main__':
     interfaces = ['dummy', 'serial']
     controller = SerialControllerInterface(port="COM5", baudrate=115200)
-
+    '''n_teve_handshake= True
+    while n_teve_handshake:
+        n_teve_handshake = controller.handshake()'''
     while True:
         controller.update()
+        
